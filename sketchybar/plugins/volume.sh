57ffb70e4a8a91a9d511eff20c895754adf0d959
Barry Saaun <panhaocr@gmail.com>
2025-01-08 17:29:11 +0700

    feat(sketchybar): modified the volume item to adjust its icon to different audio output

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -3,21 +3,34 @@
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME="$INFO"
+#  brew install switchaudio-osx to get the "SwitchAudioSource" command
 
-  case "$VOLUME" in
-  [6-9][0-9] | 100)
-    ICON="󰕾"
-    ;;
-  [3-5][0-9])
-    ICON="󰖀"
-    ;;
-  [1-9] | [1-2][0-9])
-    ICON="󰕿"
-    ;;
-  *) ICON="󰖁" ;;
-  esac
+CURRENT_OUTPUT_DEVICE="$(SwitchAudioSource -c)"
 
-  sketchybar --set "$NAME" icon="$ICON"
-fi
+case "$CURRENT_OUTPUT_DEVICE" in
+*"AirPods Pro"*) ICON="􀪷" ;;
+*"AirPods Gen 3"*) ICON="􁄡" ;;
+*"AirPods"*) ICON="􀟥" ;;
+"WH-1000XM"*) ICON="􀑈" ;;
+*)
+
+  if [ "$SENDER" = "volume_change" ]; then
+    VOLUME="$INFO"
+
+    case "$VOLUME" in
+    [6-9][0-9] | 100)
+      ICON="󰕾"
+      ;;
+    [3-5][0-9])
+      ICON="󰖀"
+      ;;
+    [1-9] | [1-2][0-9])
+      ICON="󰕿"
+      ;;
+    *) ICON="󰖁" ;;
+    esac
+  fi
+  ;;
+esac
+
+sketchybar --set "$NAME" icon="$ICON"
