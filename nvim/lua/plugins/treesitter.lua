e10b5111823c079facff522c84c8e64bb12b5d02
Barry Saaun <panhaocr@gmail.com>
2025-02-14 13:50:10 +1100

    feat(nvim): added nvim-treesitter-context plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,53 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "gitignore",
-      "graphql",
-      "http",
-      "rust",
-      "css",
-      "scss",
-      "sql",
-      "svelte",
-      "cmake",
-      "cpp",
-      "go",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "gitignore",
+        "graphql",
+        "http",
+        "rust",
+        "css",
+        "scss",
+        "sql",
+        "svelte",
+        "cmake",
+        "cpp",
+        "go",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 0,
+        trim_scope = "outer",
+        mode = "cursor",
+      })
+    end,
+    lazy = false,
+    keys = {
+      {
+        "[c",
+        function()
+          require("treesitter-context").go_to_context(vim.v.count1)
+        end,
+        desc = "Jump to treesitter context",
+      },
     },
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-
-    -- MDX
-    vim.filetype.add({
-      extension = {
-        mdx = "mdx",
-      },
-    })
-    vim.treesitter.language.register("markdown", "mdx")
-  end,
 }
