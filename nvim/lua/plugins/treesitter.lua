c6854fcb71268d3e75d36723136a6752bd9de454
Barry Saaun <panhaocr@gmail.com>
2025-02-25 15:01:31 +1100

    chore(nvim): changed max_lines of treesitter-context to be 3 instead of 0 (inf)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
     config = function()
       require("treesitter-context").setup({
         enable = true,
-        max_lines = 0,
+        max_lines = 3,
         trim_scope = "outer",
         mode = "cursor",
       })
