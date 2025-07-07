5c771bc30cc8e63602d732a41e8a3add144cca0a
Barry Saaun <panhaocr@gmail.com>
2025-07-07 13:34:00 +1000

    (nvim): keymap for staging hunk

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -31,6 +31,9 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- git staging hunk
+keymap.set("v", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+
 -- buffer management
 keymap.set("n", "<TAB>", ":bn<CR>") -- go to the next buffer
 keymap.set("n", "<S-TAB>", ":bp<CR>") -- go to the previous buffer
