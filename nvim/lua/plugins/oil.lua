ce313a1096e1d858c3bd3a09033ae4030275ee71
Barry Saaun <panhaocr@gmail.com>
2025-09-09 13:14:14 +1000

    (nvim): modified oil.nvim config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,27 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
+  opts = {
+    keymaps = {
+      -- Navigate like Netrw / NvimTree
+      ["h"] = "actions.parent",
+      ["l"] = "actions.select",
+
+      -- Open with system default program (macOS: "open", Linux: "xdg-open")
+      ["<C-o>"] = function()
+        local oil = require("oil")
+        local entry = oil.get_cursor_entry()
+        if not entry then
+          return
+        end
+        local path = oil.get_current_dir() .. entry.name
+        local opener = vim.fn.has("macunix") == 1 and "open" or "xdg-open"
+        vim.fn.jobstart({ opener, path }, { detach = true })
+      end,
+    },
+  },
   config = function(_, opts)
     require("oil").setup(opts)
-
-    vim.keymap.set("n", "<BS>", function()
-      require("oil").open("..")
-    end, { desc = "Go up directory in Oil" })
   end,
 }
