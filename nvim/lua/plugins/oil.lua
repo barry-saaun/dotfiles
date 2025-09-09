cc9a15188b3420999d29f617cc2cee73385138a1
Barry Saaun <panhaocr@gmail.com>
2025-09-09 13:30:06 +1000

    (feat): updated oil to open a floating window using "-" in Normal mode

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,21 +3,33 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
   opts = {
-    columns = {
-      "permissions",
-      "mtime",
-      "size",
-      "icon",
-    },
-    lsp_file_methods = {
-      enabled = true,
-    },
-    keymaps = {
-      -- Navigate like Netrw / NvimTree
-      ["h"] = "actions.parent",
-      ["l"] = "actions.select",
+    -- Show nice metadata view
+    columns = { "icon", "permissions", "size", "mtime" },
 
-      -- Open with system default program (macOS: "open", Linux: "xdg-open")
+    -- LSP-aware renames/moves if supported
+    lsp_file_methods = { enabled = true },
+
+    -- Floating window styling
+    float = {
+      padding = 2,
+      max_width = 0.9,
+      max_height = 0.7,
+      border = "rounded",
+      win_options = {
+        winblend = 10,
+        cursorline = true,
+      },
+    },
+
+    view_options = {
+      show_hidden = true, -- like `ls -a`
+    },
+
+    -- Keymaps inside Oil
+    keymaps = {
+      ["h"] = "actions.parent", -- go up dir
+      ["<CR>"] = "actions.select", -- open file **or** enter dir
+      ["q"] = "actions.close", -- quit Oil
       ["<C-o>"] = function()
         local oil = require("oil")
         local entry = oil.get_cursor_entry()
@@ -32,5 +44,8 @@ return {
   },
   config = function(_, opts)
     require("oil").setup(opts)
+
+    -- Open Oil in floating mode with "-"
+    vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Oil (float)" })
   end,
 }
