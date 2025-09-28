ebdf440a59809db4e855259119d8dd95d0312e3f
Barry Saaun <panhaocr@gmail.com>
2025-09-28 14:52:18 +1000

    (FIX nvim): fix on breaking changes of the new mason major changes

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,56 +1,76 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "r",
-        "c",
-        "typescript",
-        "gitignore",
-        "graphql",
-        "http",
-        "rust",
-        "css",
-        "scss",
-        "sql",
-        "svelte",
-        "cmake",
-        "cpp",
-        "go",
-        "java",
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
 
-      -- MDX
-      vim.filetype.add({
-        extension = {
-          mdx = "mdx",
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        highlight = {
+          enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "vimdoc",
+          "c",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
         },
       })
-      vim.treesitter.language.register("markdown", "mdx")
+
+      -- use bash parser for zsh files
+      vim.treesitter.language.register("bash", "zsh")
     end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("treesitter-context").setup({
-        enable = true,
-        max_lines = 3,
-        trim_scope = "outer",
-        mode = "cursor",
-      })
-    end,
-    lazy = false,
-    keys = {
-      {
-        "[c",
-        function()
-          require("treesitter-context").go_to_context(vim.v.count1)
-        end,
-        desc = "Jump to treesitter context",
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      config = function()
+        require("treesitter-context").setup({
+          enable = true,
+          max_lines = 3,
+          trim_scope = "outer",
+          mode = "cursor",
+        })
+      end,
+      lazy = false,
+      keys = {
+        {
+          "[c",
+          function()
+            require("treesitter-context").go_to_context(vim.v.count1)
+          end,
+          desc = "Jump to treesitter context",
+        },
       },
     },
   },
